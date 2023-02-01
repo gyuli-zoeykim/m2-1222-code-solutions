3eea8826f6393cf942a1cfee00d355aa640dfa53
Zoey1318 <gyulikimwork@gmail.com>
2023-02-01 03:47:57 +0000

    Completed express-delete

---
+++ express-delete/index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+var grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92
+  }
+};
+
+app.get('/api/grades', (req, res) => {
+  var arry = [];
+  for (const property in grades) {
+    var value = grades[property];
+    arry.push(value);
+  }
+  res.json(arry);
+});
+
+app.delete('/api/grades/:id', (req, res) => {
+  delete grades[req.params.id];
+  res.sendStatus(204);
+});
+
+app.listen(3000, () => {
+  /* console.log('Listening on port 3000'); */
+});
