72d5af89cc63d97a204708bb9969ffb0cc4150a7
Zoey1318 <gyulikimwork@gmail.com>
2023-02-10 03:35:06 +0000

    fixed commented code

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const data = require('./data.json');
 const fs = require('fs');
-const arry = [];
 
 app.get('/api/notes', (req, res) => {
+  const arry = [];
   for (const property in data.notes) {
     const value = data.notes[property];
     arry.push(value);
@@ -12,21 +12,18 @@ app.get('/api/notes', (req, res) => {
   res.status(200).json(arry);
 });
 
-var idArry = [];
-function getData() {
+app.get('/api/notes/:id', (req, res) => {
+  const getArry = [];
   for (const property in data.notes) {
     const idValue = data.notes[property].id;
-    idArry.push(idValue);
+    getArry.push(idValue);
   }
-}
 
-app.get('/api/notes/:id', (req, res) => {
-  getData();
   if (req.params.id < 0) {
     res.status(400).json({ error: 'id must be a positive integer' });
-  } else if (idArry.includes(Number(req.params.id)) === true) {
+  } else if (getArry.includes(Number(req.params.id)) === true) {
     res.status(200).json(data.notes[req.params.id]);
-  } else if (idArry.includes(Number(req.params.id)) === false) {
+  } else if (getArry.includes(Number(req.params.id)) === false) {
     res.status(404).json({ error: 'cannot find note with id ' + req.params.id });
   }
 });
@@ -55,12 +52,17 @@ app.post('/api/notes', (req, res) => {
 });
 
 app.delete('/api/notes/:id', (req, res) => {
-  getData();
+  const delArry = [];
+  for (const property in data.notes) {
+    const idValue = data.notes[property].id;
+    delArry.push(idValue);
+  }
+
   if (req.params.id < 0) {
     res.status(400).json({ error: 'id must be a positive integer' });
-  } else if (idArry.includes(Number(req.params.id)) === false) {
+  } else if (delArry.includes(Number(req.params.id)) === false) {
     res.status(404).json({ error: 'cannot find note with id ' + req.params.id });
-  } else if (idArry.includes(Number(req.params.id)) === true) {
+  } else if (delArry.includes(Number(req.params.id)) === true) {
     delete data.notes[req.params.id];
     fs.writeFile('./data.json', JSON.stringify(data, null, 2), err => {
       if (err) {
@@ -74,15 +76,19 @@ app.delete('/api/notes/:id', (req, res) => {
 });
 
 app.put('/api/notes/:id', (req, res) => {
-  getData();
+  const putArry = [];
+  for (const property in data.notes) {
+    const idValue = data.notes[property].id;
+    putArry.push(idValue);
+  }
   const reNote = req.body;
   if (req.params.id < 0) {
     res.status(400).json({ error: 'id must be a positive integer' });
   } else if (reNote.content === undefined) {
     res.status(400).json({ error: 'content is a required field' });
-  } else if (idArry.includes(Number(req.params.id)) === false) {
+  } else if (putArry.includes(Number(req.params.id)) === false) {
     res.status(404).json({ error: 'cannot find note with id ' + req.params.id });
-  } else if (idArry.includes(Number(req.params.id)) === true) {
+  } else if (putArry.includes(Number(req.params.id)) === true) {
     reNote.id = req.params.id;
     data.notes[req.params.id] = reNote;
     fs.writeFile('./data.json', JSON.stringify(data, null, 2), err => {
