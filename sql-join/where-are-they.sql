09c72cd2719a18c95de8ef8b50738fc315cceac5
Zoey1318 <gyulikimwork@gmail.com>
2023-02-15 01:11:09 +0000

    Completed sql-join

---
+++ sql-join/where-are-they.sql
@@ -0,0 +1,7 @@
+select "a"."line1",
+       "c"."name" as "city",
+       "a"."district",
+       "co"."name" as "country"
+  from "addresses" as "a"
+  join "cities" as "c" using ("cityId")
+  join "countries" as "co" using ("countryId")
